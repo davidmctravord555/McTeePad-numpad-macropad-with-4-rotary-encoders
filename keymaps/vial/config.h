80f127ea168027e43664d77d5da95f685016cb5d
David_McTravord555 <60816499+davidmctravord555@users.noreply.github.com>
2022-11-01 19:08:26 +0700

    Create config.h

---
+++ keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x29, 0x8A, 0x24, 0x26, 0x61, 0xBC, 0xF8, 0x28}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 2, 3 }
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
